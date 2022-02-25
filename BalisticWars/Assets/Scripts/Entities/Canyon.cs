6006622d778bc47f04518e697e0815d9d87f3d17
Javier Raja Huertas <iivaj@icloud.com>
2022-02-25 01:59:38 +0100

    preStream

--- BalisticWars/Assets/Scripts/Entities/Canyon.cs
+++ BalisticWars/Assets/Scripts/Entities/Canyon.cs
@@ -7,9 +7,11 @@ using UnityEngine;
 public class Canyon : MonoBehaviour
 {
     [SerializeField] private Transform pivot;
-    [Space] [SerializeField] private Vector2 direction;
+    [SerializeField] private Transform earth;
+    [Space] 
+    [SerializeField] private Vector2 direction;
     [SerializeField] private float angle;
-    [SerializeField] private float rotation;
+    [SerializeField] private Vector3 shootAngle;
     [SerializeField] private float rotationSpd;
     [Space] 
     [SerializeField] private GameObject proyectile;
@@ -22,18 +24,31 @@ public class Canyon : MonoBehaviour
     private void Awake()
     {
         lineRenderer = gameObject.GetComponent<LineRenderer>();
+        earth = GameObject.FindGameObjectWithTag("Earth").GetComponent<Transform>();
         GetDegRotation();
     }
     
     void Update()
     { 
-        rotation = Input.GetAxis("Horizontal");
+        var rotation = Input.GetAxis("Horizontal");
         if (rotation != 0)
         {
             pivot.Rotate(Vector3.back, rotation * rotationSpd);
             GetDegRotation();
             CalculateShoot();
         }
+
+        var potencer = Input.GetAxis("Vertical");
+        if (potencer > 0)
+        {
+            proyectileForce+=0.1f;
+            CalculateShoot();
+        } else if (potencer < 0)
+        {
+            proyectileForce-=0.1f;
+            CalculateShoot();
+        }
+        
 
         if (Input.GetButtonDown("Fire1"))
             Shoot();
@@ -43,14 +58,13 @@ public class Canyon : MonoBehaviour
 
     private void Shoot()
     {
-        var aux = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        
-        var a = new Vector2(transform.position.x, transform.position.y);
-        var b = new Vector2(aux.x, aux.y);
-        var dir = b - a;
+        //var aux = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //var a = new Vector2(transform.position.x, transform.position.y);
+        //var b = new Vector2(aux.x, aux.y);
+        //var dir = b - a;
         
         var pro = Instantiate(proyectile, pivot.position, Quaternion.identity);
-        pro.GetComponent<BalisticProyectile>().SetInitialValues(dir.normalized, dir.magnitude * proyectileForce);
+        pro.GetComponent<BalisticProyectile>().SetInitialValues(pivot.right, proyectileForce * 6.8f);
     }
 
     private void GetDegRotation()
@@ -58,23 +72,19 @@ public class Canyon : MonoBehaviour
         angle = pivot.localRotation.eulerAngles.z;
     }
 
-    private Vector3 GetVelocity(float t)
-    {
-        return pivot.up.normalized * (proyectileForce - PlayerController.gravity * t);
-    }
-    private Vector3 GetPosition(float t)
-    {
-        return pivot.position + GetVelocity(t);
-    }
-
     private void CalculateShoot()
     {
-        int j = 0;
-        for (float i = 0; i < previsualizationSteps; i+=0.1f)
+        lineRenderer.positionCount = previsualizationSteps;
+        Vector3 a = Vector3.zero; 
+        Vector3 v = pivot.right * proyectileForce; 
+        Vector3 p = pivot.position;
+        lineRenderer.SetPosition(0, p);
+        for (int i = 1; i < previsualizationSteps; i++)
         {
-            var aux = GetPosition(i);
-            lineRenderer.SetPosition(j, aux);
-            j++;
+            a = (earth.position - p).normalized * PlayerController.gravity;
+            v -= a;
+            p += v;
+            lineRenderer.SetPosition(i, p);
         }
     }
 }
